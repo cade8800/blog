fe3c5d7cf61176f684932aefbeb2f49b07fb7a04
Thornhill <cade8800@163.com>
2020-07-07 17:15:45 +0800

    blog

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,92 @@
+module.exports = {
+    base: '/blog/',
+    title: 'EEDC NOTES',
+    description: 'All a dream to begin with.Ain’t nobody ever been free.',
+
+    themeConfig: {
+        displayAllHeaders: true,
+
+        sidebar: 'auto',
+        // sidebarDepth: 2,
+
+        lastUpdated: 'Last Updated',
+        smoothScroll: true,
+
+
+        // repo: 'vuejs/vuepress',
+        // repoLabel: 'SourceCode',
+
+        // 以下为可选的编辑链接选项
+
+        // 假如你的文档仓库和项目本身不在一个仓库：
+        docsRepo: 'vuejs/vuepress111',
+        // 假如文档不是放在仓库的根目录下：
+        // docsDir: 'docs',
+        // 假如文档放在一个特定的分支下：
+        // docsBranch: 'master',
+        // 默认是 false, 设置为 true 来启用
+        editLinks: true,
+        // 默认为 "Edit this page"
+        // editLinkText: 'Edit this page',
+
+
+
+        nav: [
+            {
+                text: 'Menu',
+                items: [
+                    {
+                        text: 'Backend',
+                        items: [
+                            { text: 'Java', link: '/java/' },
+                            { text: '.Net', link: '/net/' }
+                        ]
+                    },
+                    {
+                        text: 'Front-end',
+                        items: [
+                            { text: 'Angular', link: '/angular/' },
+                            { text: 'Vue', link: '/vue/' },
+                        ]
+                    },
+                ]
+            },
+            { text: 'About', link: '/about' },
+            { text: 'Contact', link: '/contact' },
+            { text: 'GitHub', link: 'https://github.com/cade8800' },
+        ],
+
+        sidebar: [
+            {
+                title: 'Home',
+                path: '/',
+                collapsable: false,
+                sidebarDepth: 1,
+                children: [
+                    '/'
+                ]
+            },
+            {
+                title: 'Java',
+                collapsable: true,
+                children: ['/java/', ['/java/one', '没错啦'], '/java/two',]
+            },
+        ]
+
+        // sidebar: {
+        //     '/java/': [
+        //         '',     /* /foo/ */
+        //         'one',  /* /foo/one.html */
+        //         'two'   /* /foo/two.html */
+        //     ],
+        //     '/': [
+        //         '',
+        //         'contact',
+        //         'about'
+        //     ]
+        // }
+
+    },
+
+
+}
