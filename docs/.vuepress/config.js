1703c354a52be46b1fda7022f204a71c76f992e9
Thornhill <cade8800@163.com>
2020-07-07 17:27:22 +0800

    blog

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -19,11 +19,11 @@ module.exports = {
         // 以下为可选的编辑链接选项
 
         // 假如你的文档仓库和项目本身不在一个仓库：
-        docsRepo: 'vuejs/vuepress111',
+        docsRepo: 'cade8800/blog',
         // 假如文档不是放在仓库的根目录下：
-        // docsDir: 'docs',
+        docsDir: 'docs',
         // 假如文档放在一个特定的分支下：
-        // docsBranch: 'master',
+        docsBranch: 'master',
         // 默认是 false, 设置为 true 来启用
         editLinks: true,
         // 默认为 "Edit this page"
